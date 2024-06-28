9e16da7cf8dc3f3034577eef62cf40113d0a7139
Jacob David Diehl <jakediehl17@gmail.com>
2024-06-28 17:25:23 -0600

    made test for retrieving all users

--- server/src/tests/app.test.js
+++ server/src/tests/app.test.js
@@ -7,8 +7,16 @@ const app = createApp();
 
 
 
-describe("Get reports", () => {
-  it("responds to GET /reports", async () => {
+describe("Get ", () => {
+  it("responds to GET /", async () => {
     await supertest(app).get("/").expect(200);
   });
 });
+
+
+
+describe("Get users",()=>{
+  it("responds to Get /users", async()=>{
+    await supertest(app).get("/users").expect(200);
+  })
+})
